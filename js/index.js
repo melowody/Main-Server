be89fc844605c1627fe21993c3f7a6a61b237f6e
uhmily <depianoman2004@gmail.com>
2023-05-14 00:35:48 -0400

    Add Dark Mode functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,19 @@
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
 $(document).ready(function() {
     particlesJS.load('particle-wrapper', 'json/particles.json', function() {
         console.log('callback - particles.js config loaded');
     });
+
+    let stylesheet = "css/" + (getCookie("dark-mode") ? "dark" : "light") + "/index.css";
+    $("head #mode").attr("href", stylesheet);
 });
